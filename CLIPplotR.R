c8856df931a463f638b97989664915d10c684dd5
CharlotteAnne <CharlotteAnne@users.noreply.github.com>
2019-07-03 17:05:30 +0100

    added part 1 plotting
    added the crosslink plotting

--- CLIPplotR.R
+++ CLIPplotR.R
@@ -6,12 +6,14 @@ library(optparse)
 
 # TODO: Nobby to convert this list into an optparse object for final script.
 
-opt <- list(xlinks = "iclip1.bedgraph.gz,iclip2.bedgraph.gz,iclip3.bedgraph.gz",
-	gtf = "~/Ule/ref/gencode.v27.annotation.gtf.gz",
-	region = "1:123:123:+",
-	gene = "ENSG",
-	smoothing = "gaussian",
-	normalisation = "libSize",
+opt <- list(xlinks = "../tdp43_studentExercise/tardbp-esc-m-p2lox-gfp-tdp43-20151212_trimmed_single.bedgraph ../tdp43_studentExercise/tardbp-ngfp-esc-m-p2lox-gfp-tdp43-20151212_trimmed_single.bedgraph", #from https://imaps.genialis.com/iclip
+           # track_names = "tdp43_1 tdp43_2", # or can be NULL 
+	gtf = "../hg38_regions/gencode.v30.primary_assembly.annotation.gtf", #"~/Ule/ref/gencode.v27.annotation.gtf.gz"
+	region = "chr3:35754106:35856276:+",
+	gene = "ENSMUSG00000037400", #ID or name "Atp11b"
+	smoothing = "rollmean", #gaussian or rollmean
+	smoothing_window = 10, #both types of smoothing require a window
+	normalisation = "maxpeak", #libsize or maxpeak or none
 	output = "plot.pdf")
 
 # ==========
@@ -20,11 +22,10 @@ opt <- list(xlinks = "iclip1.bedgraph.gz,iclip2.bedgraph.gz,iclip3.bedgraph.gz",
 
 library(rtracklayer)
 
-region.gr <- GRanges(seqnames = Rle(sapply(strsplit(opt$region), ":"), "[[", 1),
-                     ranges = IRanges(start = Rle(sapply(strsplit(opt$region), ":"), "[[", 2),
-                                      end = Rle(sapply(strsplit(opt$region), ":"), "[[", 3)),
-                     strand = Rle(Rle(sapply(strsplit(opt$region), ":"), "[[", 4)))
-
+region.gr <- GRanges(seqnames = Rle(sapply(strsplit(opt$region, ":"), "[[", 1)),
+                     ranges = IRanges(start = as.integer(sapply(strsplit(opt$region, ":"), "[[", 2)),
+                                      end = as.integer(sapply(strsplit(opt$region, ":"), "[[", 3))),
+                     strand = Rle(sapply(strsplit(opt$region, ":"), "[[", 4)))
 
 
 # ==========
@@ -36,6 +37,38 @@ library(ggthemes)
 library(cowplot)
 library(smoother)
 library(zoo)
+library(dplyr)
+
+# Read in xlinks
+xlinks <- strsplit(opt$xlinks, " ")[[1]] %>% lapply(.,import, format="bedGRaph", which=region.gr)
+# Names of bedgraphs : If name is supplied use that, if not then generate a name from the file name
+if (!is.null(opt$track_names)) {
+  track_names=strsplit(opt$track_names, " ")[[1]]
+} else {
+  track_names=strsplit(opt$xlinks, " ")[[1]] %>% lapply(.,gsub,pattern=".bedgraph",replacement="") %>% lapply(.,gsub,pattern=".*[/@]",replacement="")
+}
+# Create dataframe for plotting
+xlinks_df <- lapply(xlinks, as.data.frame)
+for (i in 1:length(xlinks_df)){
+  xlinks_df[[i]]$sample <- track_names[[i]]
+}
+xl_df <- dplyr::bind_rows(xlinks_df)
+xl_df <- as.data.frame(xl_df)
+
+# Do the normalisation
+xl_df <- as.data.frame(switch(opt$normalisation, "libsize"=xl_df %>% dplyr::group_by(sample) %>% dplyr::mutate(norm=score/sum(score)),
+       "maxpeak"=xl_df %>% dplyr::group_by(sample) %>% dplyr::mutate(norm=score/max(score)),
+       "none"=xl_df %>% dplyr::group_by(sample) %>% dplyr::mutate(norm=score)))
+# Do the smoothing
+xl_df <- as.data.frame(switch(opt$smoothing, "gaussian"=xl_df %>% dplyr::group_by(sample) %>% dplyr::mutate(smoothed= smth.gaussian(norm, window=opt$smoothing_window)),
+                              "rollmean"=xl_df %>% dplyr::group_by(sample) %>% dplyr::mutate(smoothed= rollmean(norm, opt$smoothing_window, fill=0))))
+# Plot
+ggplot(xl_df,aes(x=start,y=smoothed, group=sample, color=sample)) +
+ geom_line() +
+  theme_few() +
+  ylab("smoothed cDNAs") +
+  xlab("chr coordinate")
+
 
 # ==========
 # Part 2 - bottom half: gene structures
